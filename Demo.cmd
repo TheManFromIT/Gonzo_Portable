56aa5aab43b9bd548a910b0a60f21a60acf66668
Dave Paterson <crazeydave.2.0@gmail.com>
2017-06-22 23:29:29 +0100

    Updated README with Demo.

---
+++ Demo.cmd
@@ -0,0 +1,4 @@
+@echo off
+echo Starting GonzoJs Demo (http://localhost:3000)
+cd GonzoJs\GonzoJs
+node app.js
---
+++ Setup.cmd
@@ -0,0 +1,4 @@
+@echo off
+echo This script sets up the GonzoJs Dependencies (Once Only Required)
+cd GonzoJs\GonzoJs
+npm install
